13370f14d9d2aaf067933854fa05931c46d9a30e
Leonid <leonigleo13@gmail.com>
2025-04-05 13:22:58 +0300

    2 step 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,27 @@
 plugins {
     id("java")
     id("org.sonarqube") version "6.0.1.5171"
+    application
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,20 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+public class App {
+
+    public static void main(String[] args) {
+        var app = getApp();
+        app.start(700);
+    }
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            });
+        app.get("/", ctx -> {
+            ctx.result("Hello World");
+        });
+        return app;
+    }
+}
